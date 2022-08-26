b54e74939e449aeb2adde0b62116528f2fd66fc2
Prathameshvardam321 <prathameshvardam321@gmail.com>
2022-08-26 13:33:21 +0530

    Added : Day 9 assignments on dictionaries

---
+++ 9.2.sh
@@ -0,0 +1,23 @@
+no_Of_Individuals=50;
+
+declare -A birth_day
+
+echo "The birthday month and year of $no_Of_Individuals individuals are: "
+
+for (( individual = 1; individual <= no_Of_Individuals; individual++ ))
+do
+   (( birth_month = RANDOM % 12 + 1 ))
+   (( birth_year = RANDOM % 2 + 92 ))
+    echo "individual $individual has birthday in month $birth_month year $birth_year "
+    birth_day[$birth_month]+=" ${individual}"
+done
+
+for month in ${!birth_day[@]}
+do
+   echo "The individuals who are having birthday in month $month are: "
+    for individual in ${birth_day[$month]}
+    do
+        	echo -n "$individual "
+   done
+   echo ""
+done
